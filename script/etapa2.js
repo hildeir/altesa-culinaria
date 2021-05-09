48ab0ea807b19a0a3b720d37c541f8258a8e6afe
Hildeir Daniel <hildeirdaniel@gmail.com>
2021-05-09 15:00:32 -0300

    ajustei os scripts

--- script/etapa2.js
+++ script/etapa2.js
@@ -2,7 +2,9 @@ window.onload = inicia;
 function inicia(){
 	let pm = sessionStorage.getItem("pm");
 	let pf = sessionStorage.getItem("pf");
-	if(pm == null && pf == null && pm == "" && pf == ""){
+	if(pm == null && pf == null){
+		location.href = "/sitetair";
+	}else if(pm == "" && pf == ""){
 		location.href = "/sitetair";
 	}else{
 		montar();
