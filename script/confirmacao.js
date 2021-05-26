863e45a48b4cf98f52ce4558301f4515811d2590
Hildeir Daniel <hildeirdaniel@gmail.com>
2021-05-25 21:44:38 -0300

    implemenntei o prato  dde prromocao na paginnaa confirmacaao

--- script/confirmacao.js
+++ script/confirmacao.js
@@ -3,11 +3,12 @@ function inicia(){
 	sessionStorage.removeItem("dados");
 	let pm = sessionStorage.getItem("pm");
 	let pf = sessionStorage.getItem("pf");
+    let pp = sessionStorage.getItem("pp");
     let cliente = sessionStorage.getItem("dadosCliente");
     let entrega = sessionStorage.getItem("dadosEntrega");
-	if(pm == null && pf == null){
+	if(pm == null && pf == null && pp == null){
 		location.href = "/sitetair";
-	}else if(pm == "" && pf == ""){
+	}else if(pm == "" && pf == "" && pp == ""){
 		location.href = "/sitetair";
 	}else if(cliente == null && entrega == null){
         location.href = "/sitetair";
@@ -27,6 +28,9 @@ function exibir(){
     const dadosCliente = JSON.parse(sessionStorage.getItem("dadosCliente"));
     const dadosEntrega = JSON.parse(sessionStorage.getItem("dadosEntrega"));
     const total = parseInt(sessionStorage.getItem("valorTotal"));
+    let pm  = sessionStorage.getItem("pm");
+    let pf = sessionStorage.getItem("pf");
+    let pp = sessionStorage.getItem("pp");
 
     const cliente = document.querySelector(".dados-cliente");
     cliente.innerHTML = "<b>nome:</b> <br/>"+
@@ -45,7 +49,41 @@ function exibir(){
                         dadosEntrega.numero +"<br/>"+
                         "<b>Complemento:</b> <br/>"+
                         dadosEntrega.complemento +"<br/>";
+    
     document.querySelector(".total").innerHTML = `Valor total R$:${total.toFixed(2)}`;
+
+    if(pm != null){
+        let array = pm.split("-");
+        array.map((ele)=>{
+            let pmjson = JSON.parse(ele);
+            const pedido = document.querySelector(".dados-pedido-pm");
+            pedido.innerHTML += "<b>prato</b><br>"+
+                            jsonMontarPratos[pmjson.id].nome+"<br>"+
+                            "<b>Quantidade:</b>"+pmjson.quantidade+"<br><br>";
+        });
+        
+    }
+    if(pf != null){
+        let array = pf.split("-");
+        array.map((ele)=>{
+            let pfjson = JSON.parse(ele);
+            const  pedido = document.querySelector(".dados-pedido-pf");
+            pedido.innerHTML += "<b>prato</b><br>"+
+                            jsonPratoFeito[pfjson.id].nome+"<br>"+
+                            "<b>Quantidade:</b>"+pfjson.quantidade+"<br><br>";
+        });
+        
+    }
+    if(pp != null){
+        let array = pp.split("-");
+        array.map((ele)=>{
+            let ppjson = JSON.parse(ele);
+            const  pedido = document.querySelector(".dados-pedido-pp");
+            pedido.innerHTML += "<b>prato</b><br>"+
+                            jsonPratoPromocao[ppjson.id].nome+"<br>"+
+                            "<b>Quantidade:</b>"+ppjson.quantidade+"<br><br>";
+        });
+    }
 
 }
 function enviar(){
@@ -54,6 +92,7 @@ function enviar(){
     let total = parseInt(sessionStorage.getItem("valorTotal"),10).toFixed(2);
     let pedidoMontado = "";
     let pedidoPratoFeito = '';
+    let pedidoPratoPromocao = ""
     if(sessionStorage.getItem('pm') != null){
         let pratos = sessionStorage.getItem("pm");
         let pratosArray = pratos.split("-");
@@ -75,16 +114,32 @@ function enviar(){
             let obj = JSON.parse(ele);
             let idInt = parseInt(obj.id,10);
             if(idInt == jsonMontarPratos[idInt].id){
-                pedidoPratoFeito += jsonMontarPratos[idInt].nome+" quant:"+obj.quantidade+"; ";
+                pedidoPratoFeito += jsonPratoFeito[idInt].nome+" quant:"+obj.quantidade+"; ";
             }
            
         });
     
     }
+    
+    /* praato de promocao */
+    if(sessionStorage.getItem('pp') != null){
+        let pratos = sessionStorage.getItem("pp");
+        let pratosArray = pratos.split("-");
+        pratosArray.map((ele)=>{
+            let obj = JSON.parse(ele);
+            let idInt = parseInt(obj.id,10);
+            if(idInt == jsonPratoPromocao[idInt].id){
+                pedidoPratoPromocao += jsonPratoPromocao[idInt].nome+" quant:"+obj.quantidade+"; ";
+            }
+           
+        });
+    
+    }
+    /* fim */
     let cliente = `Nome:${dadosCliente.nome}; email:${dadosCliente.email}; telefone:${dadosCliente.phone}`;
     let entrega = `Região:${dadosEntrega.regiao}; endereço:${dadosEntrega.ende}; Numero:${dadosEntrega.numero}; complemento:${dadosEntrega.complemento}`;
     
-    let texto = "DADOS DO PEDIDO PRATO MONTADO:"+pedidoMontado+"PEDIDO PRATO FEITOS:"+pedidoPratoFeito+"DADOS DO CLIENTE:"+cliente+"DADOS DE ENTREGA:"+entrega+"total:"+total;
+    let texto = "DADOS DO PEDIDO: PRATO MONTADO:"+pedidoMontado+"PEDIDO PRATO FEITOS:"+pedidoPratoFeito+"PEDIDO PRATO DE PROMOCAO:"+pedidoPratoPromocao+"DADOS DO CLIENTE:"+cliente+"DADOS DE ENTREGA:"+entrega+" TOTAL:"+total;
     let url = "https://api.whatsapp.com/send?phone=5521968180811&text="+texto;
     sessionStorage.clear();
     location.href = url;
