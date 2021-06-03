2953b3c92fcf9d4192795363dfc00d29af83052b
Hildeir Daniel <hildeirdaniel@gmail.com>
2021-06-03 10:33:29 -0300

    outro commit

--- script/jsonpratos.js
+++ script/jsonpratos.js
@@ -1,8 +1,8 @@
 const jsonMontarPratos = [
-	{id:0,nome:"arroz",preco:20.00,peso:"100gr",img:"imagens/vermelho.jpg",desc:"comida saborosa"},
-	{id:1,nome:"feijao",preco:30.0,peso:"100gr",img:"imagens/vermelho.jpg",desc:"comida saborosa"},
-	{id:2,nome:"frango",preco:35.0,peso:"100gr",img:"imagens/vermelho.jpg",desc:"comida saborosa"},
-	{id:3,nome:"carne vermelha",preco:55.0,peso:"100gr",img:"imagens/vermelho.jpg",desc:"comida saborosa"}
+	{id:0,nome:"arroz vermelho com raspa de limão",preco:20.00,peso:"100gr",img:"imagens/grao.jpg",desc:"comida saborosa"},
+	{id:1,nome:"purê de baroa com ervas",preco:30.0,peso:"100gr",img:"imagens/amarelo.jpg",desc:"comida saborosa"},
+	{id:2,nome:"purê de baroa com pintada de gengibre",preco:35.0,peso:"100gr",img:"imagens/pure.jpg",desc:"comida saborosa"},
+	{id:3,nome:"purê de inhame com beterraba",preco:55.0,peso:"100gr",img:"imagens/vermelho.jpg",desc:"comida saborosa"}
 ];
 
 const jsonPratoFeito = [
