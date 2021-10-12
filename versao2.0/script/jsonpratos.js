4ef27c30339fbac810aa89bc6602fd3d7a6b4414
Hildeir Daniel <hildeirdaniel@gmail.com>
2021-10-11 22:37:47 -0300

    coloquei acento no abobora

--- versao2.0/script/jsonpratos.js
+++ versao2.0/script/jsonpratos.js
@@ -8,7 +8,7 @@ const jsonMontarPratos = [
 	{id:6,nome:"carne louca de maminha",preco:125.25,peso:"100gr",img:"imagens/carne louca de maminha.jpg",desc:"comida saborosa"},
 	{id:7,nome:"frango desfiado",preco:25.32,peso:"100gr",img:"imagens/frango desfiado cremoso.jpg",desc:"comida saborosa"},
 	{id:8,nome:"abobrinha refogado na manteiga",preco:10.20,peso:"100gr",img:"imagens/abobrinha refogado na manteiga.jpg",desc:"comida saborosa"},
-	{id:9,nome:"abobora gratinada",preco:10.20,peso:"100gr",img:"imagens/abobora gratinada.jpg",desc:"comida saborosa"},
+	{id:9,nome:"abóbora gratinada",preco:10.20,peso:"100gr",img:"imagens/abobora gratinada.jpg",desc:"comida saborosa"},
 	{id:10,nome:"cenoura com ervas",preco:10.20,peso:"100gr",img:"imagens/cenoura com ervas.jpg",desc:"comida saborosa"},
 	{id:11,nome:"feijão carioca com tempero acolhedor",preco:10.20,peso:"100gr",img:"imagens/feijao carioca com tempero acolhedor.jpg",desc:"comida saborosa"},
 	{id:12,nome:"lentilha com alho poró acebolado",preco:10.20,peso:"100gr",img:"imagens/lentilha com alho poro acebolado.jpg",desc:"comida saborosa"}
