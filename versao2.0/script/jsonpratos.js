c4893ec5f4c4caf8a7e089be5daff5a8741bbeb7
Hildeir Daniel <hildeirdaniel@gmail.com>
2021-11-13 13:39:33 -0300

    adicionei novos pratos

--- versao2.0/script/jsonpratos.js
+++ versao2.0/script/jsonpratos.js
@@ -3,9 +3,16 @@ const jsonPratoFeito = [
 	{id:1,nome:"couve-flor com creme de abóbora, arroz de amendoim e feijão",preco:12.00,peso:"300gr",img:"imagens/marmitas-prontas/arroz de amendoim com creme de abóbora, couve flor e feijão - vegano.jpg",desc:"comida saborosa"},
 	{id:2,nome:"baião de dois com grão de soja e purê de batata baroa",preco:12.00,peso:"300gr",img:"imagens/marmitas-prontas/baião de 2 com grão de soja e purê de batata baroa - vegano.jpg",desc:"comida saborosa"},
 	{id:3,nome:"pene com molho branco com brócolis, queijo quale em cubos e purê de abóbora",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/pene com molho branco, brocolis, queijo quale em cubos e purê de abóbora - vegetariano.jpg",desc:"comida saborosa"},
-	{id:4,nome:"cubo de frango salteado com calda de laranja e farofa de milho com legumes",preco:15.00,peso:"300gr",img:"imagens/marmitas-prontas/cubo de frango saltiado com caldo de laranja.jpg",desc:"comida saborosa"},
-	{id:5,nome:"frango grelhado com purê de abóbora e baiao de dois",preco:15.00,peso:"300gr",img:"imagens/marmitas-prontas/peito de frango com pure de abobora e baiao de dois.jpg",desc:"comida saborosa"},
+	
+	{id:4,nome:"cubo de frango salteado com calda de laranja e farofa de milho com legumes",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/cubo de frango saltiado com caldo de laranja.jpg",desc:"comida saborosa"},
+	{id:5,nome:"frango grelhado com purê de abóbora e baiao de dois",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/peito de frango com pure de abobora e baiao de dois.jpg",desc:"comida saborosa"},
 	{id:6,nome:"carne louca de maminha com arroz integral de abóbora e legumes de brócolis e couve-flor",preco:21.90,peso:"300gr",img:"imagens/marmitas-prontas/carne louca de maminha com arroz integral de abobora e legumes de brocolis e couve-flor.jpg",desc:"comida saborosa"},
 	{id:7,nome:"Picadinho de maminha com farofa de milho com legumes gratinado",preco:21.90,peso:"300gr",img:"imagens/marmitas-prontas/Picadinho de maminha com farofa de milho com legumes gratinado.jpeg",desc:"comida saborosa"},
-	{id:8,nome:"frango desfiado cremoso com lentilha e couve-flor tostada com ervas",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/frango desfiado cremoso com lentilha e couve-flor tostada.jpeg",desc:"comida saborosa"}
+	{id:8,nome:"frango desfiado cremoso com lentilha e couve-flor tostada com ervas",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/frango desfiado cremoso com lentilha e couve-flor tostada.jpeg",desc:"comida saborosa"},
+	/* pratos novos */
+	{id:9,nome:"carne louca com arroz de brócolis acompanhado com vagem temperada com alho-poró e alho torrado",preco:21.90,peso:"300gr",img:"imagens/marmitas-prontas/carne louca com arroz de brócolis acompanhado com vagem temperada com alho-poró e alho torrado.jpg",desc:"comida saborosa"},
+	{id:10,nome:"iscas de carne acebolado com arroz cremoso de tomate e chuchu grelhado e berinjela tostada",preco:21.90,peso:"300gr",img:"imagens/marmitas-prontas/iscas de carne acebolado com arroz cremoso de tomate e chuchu grelhado e berinjela tostada.jpg",desc:"comida saborosa"},
+	{id:11,nome:"filé de tilápia com arroz de agrião acompanhado com chuchu grelhado e berinjela tostada",preco:22.90,peso:"300gr",img:"imagens/marmitas-prontas/file de tilapia com arroz de agriao acompanhado com chuchu grelhado e berinjela tostada.jpg",desc:"comida saborosa"},
+	{id:12,nome:"filé de tilápia com salada de brócolis com abrobrinha e cogumelo",preco:22.90,peso:"300gr",img:"imagens/marmitas-prontas/file de tilapia com salada de brocolis com abrobrinha e cogumelo.jpg",desc:"comida saborosa"},
+	{id:13,nome:"filé de tilápia, com arroz cremoso de tomate com vagem temperada com alho-poró e alho torrado",preco:22.90,peso:"300gr",img:"imagens/marmitas-prontas/file de tilapia com arroz cremoso de tomate com vagem temperada com alho-poro e alho torrado.jpg",desc:"comida saborosa"}
 ];
