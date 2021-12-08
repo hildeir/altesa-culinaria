fd42ad207234f05c58d07a61e654fd23d3c043e4
Hildeir Daniel <hildeirdaniel@gmail.com>
2021-12-08 16:11:59 -0300

    aumentei o valor das marmitas de frango

--- versao2.0/script/jsonpratos.js
+++ versao2.0/script/jsonpratos.js
@@ -3,9 +3,9 @@ const jsonPratoFeito = [
 	{id:1,nome:"baião de dois com grão de soja e purê de batata baroa",preco:12.00,peso:"300gr",img:"imagens/marmitas-prontas/baião de 2 com grão de soja e purê de batata baroa - vegano.jpg",desc:"comida saborosa"},
 	{id:2,nome:"pene com molho branco com brócolis, queijo coalho em cubos e purê de abóbora",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/pene com molho branco, brocolis, queijo quale em cubos e purê de abóbora - vegetariano.jpg",desc:"comida saborosa"},
 	
-	{id:3,nome:"cubo de frango salteado com calda de laranja e farofa de milho com legumes",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/cubo de frango saltiado com caldo de laranja.jpg",desc:"comida saborosa"},
-	{id:4,nome:"frango grelhado com purê de abóbora e feijão tropeiro de frango",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/frango grelhado com purê de abóbora e feijão tropeiro de frango.jpg",desc:"comida saborosa"},
-	{id:5,nome:"frango desfiado cremoso com lentilha e couve-flor tostada com ervas",preco:17.00,peso:"300gr",img:"imagens/marmitas-prontas/frango desfiado cremoso com lentilha e couve-flor tostada.jpeg",desc:"comida saborosa"},
+	{id:3,nome:"cubo de frango salteado com calda de laranja e farofa de milho com legumes",preco:17.90,peso:"300gr",img:"imagens/marmitas-prontas/cubo de frango saltiado com caldo de laranja.jpg",desc:"comida saborosa"},
+	{id:4,nome:"frango grelhado com purê de abóbora e feijão tropeiro de frango",preco:17.90,peso:"300gr",img:"imagens/marmitas-prontas/frango grelhado com purê de abóbora e feijão tropeiro de frango.jpg",desc:"comida saborosa"},
+	{id:5,nome:"frango desfiado cremoso com lentilha e couve-flor tostada com ervas",preco:17.90,peso:"300gr",img:"imagens/marmitas-prontas/frango desfiado cremoso com lentilha e couve-flor tostada.jpeg",desc:"comida saborosa"},
 	{id:6,nome:"carne louca com arroz integral de abóbora e legumes de brócolis e couve-flor",preco:21.90,peso:"300gr",img:"imagens/marmitas-prontas/carne louca de maminha com arroz integral de abobora e legumes de brocolis e couve-flor.jpg",desc:"comida saborosa"},
 	{id:7,nome:"Picadinho de maminha com farofa de milho com legumes gratinado",preco:21.90,peso:"300gr",img:"imagens/marmitas-prontas/Picadinho de maminha com farofa de milho com legumes gratinado.jpeg",desc:"comida saborosa"},
 
